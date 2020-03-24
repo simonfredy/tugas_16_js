9d83f386b227e0c9184f22f2816d312b7654e78d
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-24 19:52:54 +0700

    Tugas 16 JS

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+function tugasEnamBelas(){
+    var nMin = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    var nMax = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+    console.log('Nilai Minimal : ', nMin);
+    console.log('Nilai Maksimal : ', nMax);
+}
+tugasEnamBelas();
